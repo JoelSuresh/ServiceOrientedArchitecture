3e1241943fa920601ecc5949938feb1bb44c769e
JoelSuresh <monujo7@gmail.com>
2023-10-31 12:23:49 +0530

    services and gateway

---
+++ student/src/main/java/com/project/student/StudentService.java
@@ -0,0 +1,26 @@
+package com.project.student;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    public void saveStudent(Student student) {
+        repository.save(student);
+    }
+
+    public List<Student> findAllStudents() {
+        return repository.findAll();
+    }
+
+    public List<Student> findAllStudentsBySchool(Integer schoolId) {
+        return repository.findAllBySchoolId(schoolId);
+    }
+}
